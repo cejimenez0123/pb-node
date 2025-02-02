2dc7852418b36b8a8e30a4306636fc69de67dbc8
cejimenez0123 <christianjimenez0123@gmail.com>
2025-02-02 17:13:49 -0500

    update

--- newsletter/index.js
+++ newsletter/index.js
@@ -1,32 +1,49 @@
 const prisma = require('../db')
 const nodemailer = require('nodemailer');
 const jwt = require("jsonwebtoken")
+
 const requestSubscriptionToNewsletter = require("../html/requestSubscriptionToNewsLetter")
-const sendEmail=async (email )=>{
+const sendEmail=async (user)=>{
    
     let transporter = nodemailer.createTransport({
         service: 'gmail', 
         auth: {
-          user: email, 
+          user: process.env.pbEmail, 
           pass: process.env.pbPassword
         },
-        from:email
+        from:process.env.pbEmail
       });
 
-     let user = await prisma.user.findMany({where:{
-        email:{
-            equals:email
-        }
-      }})
-      user = user[0]
+     
       if(user){
       const token = jwt.sign({ userId:user.id }, process.env.JWT_SECRET);
-     let mailOptions = requestSubscriptionToNewsletter({token,email})
+     let mailOptions = requestSubscriptionToNewsletter({token,email:user.email})
      await transporter.sendMail(mailOptions);
      console.log("Success news")
       }else{
         console.log("Can find user")
-        return "Can't find user"
+        
       }
 }
-sendEmail().then(res=>console.log(res))
+const sendEmails=async ()=>{
+
+    const users = await prisma.user.findMany()
+
+for(const user of users){
+    await sleep(1000)
+    sendEmail(user).then(res=>{
+        console.log(user.email)
+    })
+}
+     
+          
+}  
+
+// sendEmails().then()
+
+
+// function sleep(ms) {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, ms);
+//   });
+// }
