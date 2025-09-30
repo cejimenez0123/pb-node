ab051a7584dcee62cae8dfcffc3916f38a5d8149
cejimenez0123 <christianjimenez0123@gmail.com>
2025-09-30 16:46:52 -0400

    update prisma error syntax

--- newsletter/sendEventNewsletterEmail.js
+++ newsletter/sendEventNewsletterEmail.js
@@ -2,7 +2,7 @@ const prisma = require("../db");
 const eventNewsletterTemplate = require("../html/eventNewsletterTemplate");
 const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
-const prisma = require("../db");
+
 module.exports = async function  sendEventNewsletterEmail(user,events,days=7){
   try {
     const template = eventNewsletterTemplate(events,user,days);
