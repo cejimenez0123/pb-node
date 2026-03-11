0ad6720f985426494c10ccaa77bd65cfbbc08487
cejimenez0123 <christianjimenez0123@gmail.com>
2026-03-11 13:27:53 -0400

    update

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 console.log('Prisma initialized');
 module.exports = prisma
